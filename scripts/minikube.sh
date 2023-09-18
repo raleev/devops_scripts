0e1032406faf2363e2809ef59d49d51102663432
raleev <mina@naveenmannam.com>
2023-09-17 20:15:23 -0400

    Adding the minikube start command in the file

--- scripts/minikube.sh
+++ scripts/minikube.sh
@@ -3,3 +3,4 @@
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
 
+# minikube start --vm-driver=docker --memory=6G --cni=calico
