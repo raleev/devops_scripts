42e915fe179760f685bbc1c1a0eac0f4b700fa95
mialeevs <mialeevs@pm.me>
2023-11-13 15:25:19 -0600

    Adding Maven installation files
    Signed-off-by: mialeevs <mialeevs@pm.me>

---
+++ scripts/Maven/maven_fedora.sh
@@ -0,0 +1,4 @@
+sudo dnf install java-11-openjdk.x86_64
+java -version
+sudo dnf install -y maven
+mvn -v
---
+++ scripts/Maven/maven_ubuntu.sh
@@ -0,0 +1,4 @@
+sudo apt install openjdk-11-jdk -y
+java -version
+sudo apt install -y maven
+mvn -v
