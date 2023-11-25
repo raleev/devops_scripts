8a2e2fb95ec65673b95b04051b6047cb3a7a0351
raleev <mina@naveenmannam.com>
2023-11-25 17:38:01 -0500

    Adding pbcopy script

--- scripts/Terraform/pbcopy.sh
+++ scripts/Terraform/pbcopy.sh
@@ -4,5 +4,5 @@
 
 sudo apt-get install xclip -y
 
-# alias pbcopy=’xclip -selection clipboard’
-# alias pbpaste=’xclip -selection clipboard -o’
+# alias pbcopy="xclip -selection clipboard"
+# alias pbpaste="xclip -selection clipboard -o"
